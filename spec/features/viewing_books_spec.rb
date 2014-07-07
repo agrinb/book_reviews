514f9101df83d7fab3927e615d6e6c1ed04f2eed
Helen Hood <helen.c.hood@gmail.com>
2014-07-07 10:16:26 -0400

    Remove unnecessary test

--- spec/features/viewing_books_spec.rb
+++ spec/features/viewing_books_spec.rb
@@ -5,7 +5,6 @@ feature 'user is looking for a book', %Q{
 } do
 
   scenario 'user looks at list of books' do
-
     book1 = FactoryGirl.create(:book)
     book2 = FactoryGirl.create(:book)
     book3 = FactoryGirl.create(:book)
@@ -15,15 +14,4 @@ feature 'user is looking for a book', %Q{
     expect(page).to have_content book2.author
     expect(page).to have_content book3.year
   end
-
-  scenario 'user looks at list of books' do
-    book1 = FactoryGirl.create(:book)
-
-
-    visit(book_path(book1))
-    expect(page).to have_content book1.title
-    expect(page).to have_content book1.author
-  end
-
-
 end
